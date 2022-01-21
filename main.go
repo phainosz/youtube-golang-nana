59104b9bfcdd9545139bffcb58b8fb06143d6337
Paulo Hainosz <phshainosz@hotmail.com>
2022-01-21 17:50:07 -0300

    Hello World using Golang

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// in order to start a few steps has to happen:
+// start a module for the application, ex: go mod init MODULE_NAME
+// add package
+// create the main function
+// to run, open the terminal and type: go run main.go
