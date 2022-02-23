420c5fb295fdad8c2ef28990c132982004c9e7f3
Paulo Hainosz <phshainosz@hotmail.com>
2022-02-23 00:01:31 -0300

    Criado package, map e separaçao de funçoes

--- annotations.md
+++ annotations.md
@@ -20,3 +20,104 @@
 ### Ponteiros
 - Usar & para apontar para o local em memória de uma variável
 - Exemplo de uso, ponteiro para adicionar valor em variável usando Scan: `fmt.Scan(&myVar)`
+
+### Arrays
+- Sintaxe: `var array = [50]string{}` ou `var array [50]string`
+- Para adicionar, é da mesma forma convencional: `array[0] = "String"`
+- Tamanho total do array ex: `len(array)`
+
+### Slices
+- É uma abstração do array com tamanho dinâmico
+- Criado igual o array mas sem o tamanho do array, ex: `var slice[]string` ou `var slice = []string{}`
+- Para adicionar no próximo elemento do slice, é utilizado append, ex: `slice = append(slice, "Dado")`
+
+### Maps
+- É uma collection de chave valor
+- Criado usando chave e valor tipados, ex: `var mymap map[string]string` ou um map vazio `var mymap = make(map[string]string)`
+- Para adicionar no map, é utilizado append, ex: `mymap["key"] = value`
+
+
+### Loops
+- Apenas for existe no Go, while, doWhile não existem
+- O mesmo de C ou Java, ex: 
+```
+sum := 0
+for i := 1; i < 5; i++ {
+    sum += i
+}
+```
+- Maneira que se assemelha ao whie, ex: 
+```
+n := 1
+for n < 5 {
+    n *= 2
+}
+```
+- Loop infinito, ex:
+```
+sum := 0
+for {
+    sum++ 
+}
+```
+- ForEach range loop, ex:
+```
+strings := []string{"hello", "world"}
+for index, element := range strings {
+    fmt.Println(i, s)
+}
+```
+- Em casos onde o index do ForEach não é necessário, pode utilizar _ para ignorar o indice, ex:
+```
+strings := []string{"hello", "world"}
+for _, element := range strings {
+    fmt.Println(i, s)
+}
+```
+### Condições IFs e Switch
+- Seguem o mesmo formato, apenas muda o fato de usar () após o if, ex:
+```
+if someVariable {
+
+} else {
+
+}
+```
+- Para o switch, mesma coisa, ex:
+```
+switch someVariable {
+    case 1:
+        //do something
+    case 2, 3, 4:
+        //do something
+    default:
+        //do default
+}
+```
+### Funções
+- A nomenclatura usada no Go é func.
+- Funções sem retorno, ex:
+```
+func someFunction() {
+    //do something
+}
+```
+- Funções com retorno, ex:
+```
+func someFunction() string {
+    //do something
+    return someString
+}
+```
+- No Golang, pode ser retornado múltiplos valores, ex:
+```
+func someFunction() (string, int) {
+    //do something
+    return someString, someInt
+}
+```
+
+### Packages
+- O pacote main é o principal como o nome já diz. Podendo ser utilizado com varios arquivos .go neste mesmo pacote, a diferença está na forma de rodar, os arquivos precisam ser declarados no `go run main.go xxx.go yyy.go` ou na raiz `go run .`.
+- Novos pacotes podem ser criado com outras pastas com o nome do pacote.
+- Para fazer o import de uma função ou variáveis de um pacote diferente, basta declarar o nome da função como pascal case.
--- main.go
+++ main.go
@@ -1,31 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"booking-app/helper"
+	"fmt"
+	"strconv"
+)
+
+const conferenceTickets int = 50
+
+var remainingTickets uint = 50
+var conferenceName = "Go Conference"
+var bookings = make([]map[string]string, 0)
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets uint = 50
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
-	fmt.Println("Get you tickets here to attend")
+	greetUsers()
 
-	var userName string
-	var userTickets uint
+	for {
+
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			bookTicket(userTickets, firstName, lastName, email)
+
+			firstNames := printFirstNames()
+			fmt.Printf("The first names %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("firt name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+			continue
+		}
+	}
+}
+
+func printFirstNames() []string {
+	firstNames := []string{}
+
+	for _, booking := range bookings {
+		firstNames = append(firstNames, booking["firstName"])
+	}
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
 	var email string
+	var userTickets uint
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&userName)
+	fmt.Println("Enter Your First Name: ")
+	fmt.Scan(&firstName)
 
-	fmt.Print("Enter your email address: ")
+	fmt.Println("Enter Your Last Name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter Your Email: ")
 	fmt.Scan(&email)
 
-	fmt.Print("Enter number of tickets: ")
+	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	return firstName, lastName, email, userTickets
+}
+
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("Thank you %v for booking %v tickets. You will receive a confirmation email at %v\n", userName, userTickets, email)
-	fmt.Printf("%v tockets remaining for %v\n", remainingTickets, conferenceName)
+	// create user map
+	var user = make(map[string]string)
+	user["firstName"] = firstName
+	user["lastName"] = lastName
+	user["email"] = email
+	user["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+	bookings = append(bookings, user)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
